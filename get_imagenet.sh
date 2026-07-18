53af87abca6516c6012407b4d4de7736c869d2e3
MING-YEN LEE <mlee838@gatech.edu>
2026-07-18 16:33:14 +0800

    Copy training data instead of moving it
    Changed 'mv' to 'cp' for ILSVRC2012_img_train.tar to preserve the original file.

--- get_imagenet.sh
+++ get_imagenet.sh
@@ -11,7 +11,7 @@ wget https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_train.tar
 wget https://image-net.org/data/ILSVRC/2012/ILSVRC2012_devkit_t12.tar.gz
 
 ## 2. Extract the training data:
-mkdir train && mv ILSVRC2012_img_train.tar train/ && cd train
+mkdir train && cp ILSVRC2012_img_train.tar train/ && cd train
 tar -xvf ILSVRC2012_img_train.tar && rm -f ILSVRC2012_img_train.tar
 find . -name "*.tar" | while read NAME ; do mkdir -p "${NAME%.tar}"; tar -xvf "${NAME}" -C "${NAME%.tar}"; rm -f "${NAME}"; done
 cd ..
